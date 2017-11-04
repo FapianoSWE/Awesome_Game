29b1b78176df4cce2c0be8c3dc2271de1196f7d8
Fabian Haglund <fabian.haglund@learnet.se>
2017-11-04 12:00:13 +0100

    Checkpoint
    Added basic checkpoint functonality

--- Awesome Game/Assets/Scripts/PlayerController.cs
+++ Awesome Game/Assets/Scripts/PlayerController.cs
@@ -6,28 +6,51 @@ public enum Gamestates
     dead,
     alive
 }
+public enum Characters
+{
+    happy,
+    sad,
+    sleepy,
+    angery
+}
+
 
 
 public class PlayerController : MonoBehaviour {
 
+    public GameObject currentCheckpoint;
 
     public float speed = 10f;
-    public float jumpHeight = 20f;
+    public float jumpHeight = 10f;
+    public float angeryJump = 5f;
     public bool grounded = false;
+    public bool canSetJump;
     public Vector2 currentVelocity;
     Rigidbody2D rb;
 
-    Gamestates gameStates = new Gamestates();
+    public Gamestates gameStates = new Gamestates();
 
-    public GameObject[] currentTeam;
 
-	void Start ()
+    public Characters characters = new Characters();
+
+    void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
+        characters = Characters.happy;
+        gameStates = Gamestates.alive;
+
 	}
 	
 	void Update ()
     {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            gameStates = Gamestates.dead;
+        }
+        if(gameStates == Gamestates.dead)
+        {
+            Respawn();
+        }
         currentVelocity = rb.velocity;
 
         if (Input.GetKeyDown(KeyCode.R))
@@ -38,6 +61,21 @@ public class PlayerController : MonoBehaviour {
         if(rb.velocity.y > 0 || rb.velocity.y < 0)
         {
             grounded = false;
+        }
+        if(characters == Characters.happy)
+        {
+            if(canSetJump)
+            {
+                jumpHeight *= 2;
+                canSetJump = false;
+            }
+        }
+        else if(characters == Characters.angery)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                AngerySkill();
+            }
         }
     }
 
@@ -50,7 +88,7 @@ public class PlayerController : MonoBehaviour {
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
         }
@@ -66,6 +104,34 @@ public class PlayerController : MonoBehaviour {
         {
             grounded = true;
         }
+        if(collision.collider.tag == "checkpoint")
+        {
+            currentCheckpoint = collision.gameObject;
+            
+        }
     }
 
+    void AngerySkill()
+    {
+        rb.velocity = new Vector2(0, angeryJump);
+        if(rb.velocity.y == 0 && !grounded)
+        {
+            rb.velocity = new Vector2(0, -angeryJump * 2);
+        }
+    }
+    
+    void Respawn()
+    {
+        rb.velocity = Vector2.zero;
+        if(currentCheckpoint != null)
+        {
+            transform.position = currentCheckpoint.transform.position;
+            gameStates = Gamestates.alive;
+        }
+        else if(currentCheckpoint == null)
+        {
+            transform.position = GameObject.Find("Spawn").transform.position;
+        }
+            
+    }
 }
