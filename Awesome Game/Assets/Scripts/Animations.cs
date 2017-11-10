2bc082055ab7f7b218aa404324d6c8aecfde6c73
FapianoSWE <33018606+FapianoSWE@users.noreply.github.com>
2017-11-10 15:19:55 +0100

    Put the files back in place

---
+++ Awesome Game/Assets/Scripts/Animations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour {
+
+    public Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+
+	void Update ()
+    {
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Player"))
+        {
+                g.GetComponentInChildren<Animator>().SetBool("IsMoving", g.GetComponent<PlayerController>().isMoving);
+        }
+	}
+}
